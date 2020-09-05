c035064ec23654c2155618ed97312b4a5dd7a095
kolterdyx <ciruelo@gmail.com>
2020-09-05 19:16:39 +0100

    Added loading and saving settings and map saving

--- map_editor.py
+++ map_editor.py
@@ -14,6 +14,9 @@ import numpy as np
 from multiprocessing import Process, Manager, Value, Array
 from threading import Thread
 from concurrent.futures import ThreadPoolExecutor
+import pygame as pg
+
+vec = pg.math.Vector2
 
 manager = Manager()
 
@@ -26,6 +29,7 @@ reset_settings = Value('i', 0)
 generation_finished = Value('i', 0)
 generation_started = Value('i', 0)
 chunks_generated = Value('i', 0)
+apply_settings = Value('i', 0)
 
 
 SETTINGS["oct"] = 1
@@ -35,12 +39,11 @@ SETTINGS["lac"] = 2
 SETTINGS["seed"] = 0
 SETTINGS["wdh"] = 600
 SETTINGS["hgt"] = 600
-SETTINGS["pos"] = [0, 0]
+SETTINGS["offset"] = [0, 0]
 SETTINGS["mwd"] = 600
 SETTINGS["mhg"] = 600
-SETTINGS["xof"] = 0
-SETTINGS["yof"] = 0
 SETTINGS["zoom"] = 0
+SETTINGS["zoom_slider"] = 0
 SETTINGS["noise"] = 2
 
 SHARED_MAP = manager.dict()
@@ -73,15 +76,15 @@ def create_noise(iterations, x, y, scale, lacunarity, low, high, ngen):
     return int(high-noise)
 
 
-def draw_colored(octv, scale, sea, width, height, lac, pos, seed, noise):
+def draw_colored(octv, scale, sea, width, height, lac, pos, seed, noise, offset):
 
     from noise import pnoise2, snoise2
     from opensimplex import OpenSimplex
     import numpy as np
 
     generation_started.value = 1
-    xoffset = pos[0]
-    yoffset = pos[1]
+    xoffset = offset[0] + pos[0]
+    yoffset = offset[1] + pos[1]
     world = np.full((height, width, 3), 255, dtype=np.uint8)
     normal_world = world.copy()
     # Color all pixels
@@ -96,7 +99,7 @@ def draw_colored(octv, scale, sea, width, height, lac, pos, seed, noise):
                                   octaves=octv, base=seed, lacunarity=lac), -1, 1, 0, 100)
             elif noise == 0:
                 ngen = OpenSimplex(seed)
-                n = create_noise(octv, x, y, scale, lac, 0, 100, ngen)
+                n = create_noise(octv, x+xoffset, y+yoffset, scale, lac, 0, 100, ngen)
 
             color_dist = int((100-sea)/(octv+2))
 
@@ -148,6 +151,8 @@ def sw():
 
     import pygame as pg
     from pgui import Slider, Entry, Button, CheckBox
+    import json
+    import os
 
     pg.init()
     pg.display.set_caption("Settings")
@@ -181,7 +186,7 @@ def sw():
 
         def update(self):
             self.group = self.parent.noise_options
-            if self.box._clicked:
+            if self.box.clicked:
                 self.checked = True
                 for c in self.group:
                     if c != self:
@@ -224,7 +229,7 @@ def sw():
             self.box.set_style(style)
 
         def set_label(self, text):
-            self.box.set_text(text)
+            self.box.set_label(text)
 
     class Screen:
         def create(self):
@@ -247,25 +252,30 @@ def sw():
             self.reset_button.set_font_size(15)
 
             self.opensimplex_option = CustomCheckBox(self, x=20, y=20)
-            self.opensimplex_option.check_color = 0
+            self.opensimplex_option.check_color = (0, 0, 0)
             self.opensimplex_option.set_font_size(15)
             self.opensimplex_option.set_label("OpenSimplex noise")
             self.opensimplex_option.text_side = "right"
             self.opensimplex_option.label_padding = 5
 
             self.perlin_noise_option = CustomCheckBox(self, x=20, y=50)
-            self.perlin_noise_option.check_color = 0
+            self.perlin_noise_option.check_color = (0, 0, 0)
             self.perlin_noise_option.set_font_size(15)
             self.perlin_noise_option.set_label("Perlin noise")
             self.perlin_noise_option.text_side = "right"
             self.perlin_noise_option.label_padding = 5
 
             self.simplex_option = CustomCheckBox(self, x=20, y=80)
-            self.simplex_option.check_color = 0
+            self.simplex_option.check_color = (0, 0, 0)
             self.simplex_option.set_font_size(15)
             self.simplex_option.set_label("Simplex noise")
             self.simplex_option.text_side = "right"
             self.simplex_option.label_padding = 5
+
+            self.settings_file = Entry(self, x=20, y=500, border=1, size=15, width=300)
+            self.settings_file.width = 300
+            self.settings_file.text = "settings.json"
+            self.settings_file.set_label("Settings file:")
 
             self.noise_options = [
                 self.opensimplex_option,
@@ -281,17 +291,37 @@ def sw():
                 self.reset_button,
                 self.opensimplex_option,
                 self.perlin_noise_option,
-                self.simplex_option
+                self.simplex_option,
+                self.settings_file
             ]
 
             for w in self.widgets:
                 w.border_width = 1
 
         def load(self):
-            pass
+            filename = self.settings_file.text
+            if os.path.isfile(filename):
+                with open(filename, 'r') as f:
+                    settings = json.load(f)
+
+                SETTINGS.update(settings)
+
+                self.apply_settings()
+                self.settings_file.set_label("Settings file:")
+            else:
+                self.settings_file.set_label("Settings file:    (file not found)")
 
         def save(self):
-            pass
+            settings = SETTINGS.copy()
+            filename = "settings.json"
+            with open(filename, 'w') as f:
+                json.dump(settings, f)
+
+        def apply_settings(self):
+            apply_settings.value = 1
+            for i in self.noise_options:
+                i.checked = False
+            self.noise_options[SETTINGS["noise"]].checked = True
 
         def reset(self):
             reset_settings.value = 1
@@ -348,6 +378,7 @@ def editor():
     import pygame as pg
     from pgui import Slider, Entry, Button, CheckBox
     import os
+    import cv2
 
     pg.init()
 
@@ -428,22 +459,6 @@ def editor():
             self.lac_slider.set_label("Lacunarity: 2")
             self.lac_slider.set_mark(10)
 
-            self.x_offset_slider = Slider(self, max=SETTINGS["wdh"])
-            self.x_offset_slider.move(20, 280)
-            self.x_offset_slider.set_length(300)
-            self.x_offset_slider.set_font_size(15)
-            self.x_offset_slider.set_width(15)
-            self.x_offset_slider.set_label("X Padding: 0")
-            self.x_offset_slider.set_mark(SETTINGS["wdh"]/2)
-
-            self.y_offset_slider = Slider(self, max=SETTINGS["hgt"])
-            self.y_offset_slider.move(20, 320)
-            self.y_offset_slider.set_length(300)
-            self.y_offset_slider.set_font_size(15)
-            self.y_offset_slider.set_width(15)
-            self.y_offset_slider.set_label("Y Padding: 0")
-            self.y_offset_slider.set_mark(SETTINGS["hgt"]/2)
-
             self.seed_entry = Entry(self, x=20, y=360, border=1, size=15)
             self.seed_entry.text = '0'
             self.seed_entry.set_label("Seed:")
@@ -461,6 +476,10 @@ def editor():
             self.abort_button.height = 25
             self.abort_button.set_font_size(15)
 
+            self.save_button = Button(self, x=260, y=570, text='Save', func=self.save)
+            self.save_button.height = 25
+            self.save_button.set_font_size(15)
+
             self.widgets = [
                 self.oct_slider,
                 self.sea_level_slider,
@@ -471,21 +490,41 @@ def editor():
                 self.height_slider,
                 self.pos_entry,
                 self.lac_slider,
-                self.x_offset_slider,
-                self.y_offset_slider,
                 self.seed_entry,
                 self.clear_button,
                 self.settings_button,
-                self.abort_button
+                self.abort_button,
+                self.save_button
             ]
+
+            self.width_chunks = 4
+            self.height_chunks = 4
 
             for w in self.widgets:
                 w.border_width = 1
                 if isinstance(w, Slider):
                     w.pointer_border_width = 1
 
+            self.movable_map_rect = pg.Rect((0, 0), (600, 600))
+            self.dragging = False
+            self.dist = vec(0, 0)
+
         def settings(self):
             start_settings.value = 1
+
+        def save(self):
+            filename = "image.png"
+            surf = pg.transform.rotate(self.map, 90)
+            surf = pg.transform.flip(surf, 0, 1)
+            image = []
+            for x in range(SETTINGS["mwd"]):
+                row = []
+                for y in range(SETTINGS["mhg"]):
+                    row.append(surf.get_at((x, y)))
+                image.append(row)
+            image = np.array(image)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            cv2.imwrite(filename, image)
 
         def abort_generation(self):
             if generation_started.value:
@@ -499,27 +538,30 @@ def editor():
             SETTINGS["mwd"] = SETTINGS["wdh"]
             SETTINGS["mhg"] = SETTINGS["hgt"]
 
+            SHARED_MAP.clear()
+            chunks_generated.value = 0
+
             self.draw_processes = []
 
-            for x in range(10):
-                for y in range(10):
-                    width = int(SETTINGS["wdh"]/10)
-                    height = int(SETTINGS["hgt"]/10)
+            for x in range(self.width_chunks):
+                for y in range(self.height_chunks):
+                    width = int(SETTINGS["wdh"]/self.width_chunks)
+                    height = int(SETTINGS["hgt"]/self.height_chunks)
                     pos = [x*width, y*height]
 
                     self.draw_processes.append(Process(target=draw_colored, args=(SETTINGS["oct"], SETTINGS["scl"], SETTINGS["sea"], width,
                                                                                   height, SETTINGS["lac"], pos, SETTINGS["seed"],
-                                                                                  SETTINGS["noise"])))
+                                                                                  SETTINGS["noise"], SETTINGS["offset"])))
 
             for p in self.draw_processes:
                 p.start()
+                p.join()
 
         def clear(self):
             self.map.fill((100, 100, 100))
             self.map_surface.fill(0)
-            if chunks_generated.value == 100:
-                SHARED_MAP.clear()
-                chunks_generated.value = 0
+            SHARED_MAP.clear()
+            chunks_generated.value = 0
 
         def run(self):
 
@@ -535,16 +577,40 @@ def editor():
             self.width_slider.set_mark(50)
             self.height_slider.set_mark(50)
             self.lac_slider.set_mark(10)
-            self.x_offset_slider.set_mark(300)
-            self.y_offset_slider.set_mark(300)
             self.pos_entry.text = '0,0'
             self.seed_entry.text = '0'
+            self.mmrect.topleft = self.map_rect.topleft
+            self.movable_map_rect.topleft = (0, 0)
+            self.dist = vec(0, 0)
+
+        def update_settings(self):
+
+            self.oct_slider.set_mark(SETTINGS["oct"]-1)
+            self.sea_level_slider.set_mark(SETTINGS["sea"])
+            self.scale_slider.set_mark(SETTINGS["scl"]-1)
+            self.zoom_slider.set_mark(SETTINGS["zoom_slider"])
+            self.zoom = SETTINGS["zoom"]
+            self.width_slider.set_mark(int((SETTINGS["wdh"]-100)/10))
+            self.height_slider.set_mark(int((SETTINGS["hgt"]-100)/10))
+            pos = SETTINGS["offset"]
+            x = str(pos[0])
+            y = str(pos[1])
+            self.pos_entry.text = x+','+y
+            self.lac_slider.set_mark(SETTINGS["lac"]*10-10)
+            self.seed_entry.text = str(SETTINGS["seed"])
 
         def update(self):
+            mousepos = vec(pg.mouse.get_pos())
+            p1 = pg.mouse.get_pressed()[0]
+            # print(p1, mousepos)
             self.screen.fill((200, 200, 200))
             if reset_settings.value:
                 reset_settings.value = 0
                 self.reset_settings()
+
+            if apply_settings.value:
+                apply_settings.value = 0
+                self.update_settings()
 
             if generation_finished.value:
                 generation_finished.value = 0
@@ -552,7 +618,7 @@ def editor():
                 width = len(SHARED_MAP.values()[0])
                 height = len(SHARED_MAP.values()[0][0])
 
-                self.map = pg.Surface((width*10, height*10))
+                self.map = pg.Surface((width*self.width_chunks, height*self.height_chunks))
                 self.map.fill((100, 100, 100))
                 self.map_surface.fill((100, 100, 100))
 
@@ -564,12 +630,27 @@ def editor():
 
                 self.map = pg.transform.flip(pg.transform.rotate(self.map, -90), 1, 0)
 
-            self.map_surface.fill((0, 0, 0))
-            temp_map = pg.transform.scale(self.map, (int(SETTINGS["mwd"]*self.zoom), int(SETTINGS["mhg"]*self.zoom)))
-            temp_map_rect = temp_map.get_rect()
-            temp_map_rect.center = (300+SETTINGS["xof"]*self.zoom,
-                                    300+SETTINGS["yof"]*self.zoom)
-            self.map_surface.blit(temp_map, temp_map_rect)
+            self.movable_map = pg.transform.scale(
+                self.map, (int(SETTINGS["mwd"]*self.zoom), int(SETTINGS["mhg"]*self.zoom)))
+            self.movable_map_rect.size = self.movable_map.get_rect().size
+            self.mmrect = self.movable_map_rect.copy()
+            self.mmrect.topleft = vec(self.mmrect.topleft) + vec(575, 25)
+
+            if self.mmrect.collidepoint(mousepos) and self.map_rect.collidepoint(mousepos):
+                mpos = vec(mousepos) - vec(575, 25)
+
+                if p1 and not self.dragging:
+                    self.dragging = True
+                    self.dist = mousepos - vec(self.mmrect.topleft)
+
+                elif self.dragging and p1:
+                    self.mmrect.topleft = mousepos - self.dist
+                    self.movable_map_rect.center = vec(self.mmrect.center) - vec(575, 25)
+                elif self.dragging and not p1:
+                    self.dragging = False
+
+            self.map_surface.fill(0)
+            self.map_surface.blit(self.movable_map, self.movable_map_rect)
             self.screen.blit(self.map_surface, self.map_rect)
             pg.draw.rect(self.screen, (0, 0, 0), self.map_rect, 1)
             for w in self.widgets:
@@ -592,8 +673,12 @@ def editor():
 
             self.zoom_slider.set_label("Zoom: "+str(self.zoom_slider.max-self.zoom_slider.mark-25))
             self.zoom = (self.zoom_slider.max-self.zoom_slider.mark)/25
+            SETTINGS["zoom_slider"] = self.zoom_slider.mark
             if self.zoom <= 0:
                 self.zoom = 0.025
+
+            SETTINGS["zoom"] = self.zoom
+            # print(self.zoom)
 
             self.width_slider.set_label("Width: "+str((self.width_slider.mark)*10+100))
             SETTINGS["wdh"] = (self.width_slider.mark)*10+100
@@ -603,11 +688,6 @@ def editor():
             self.lac_slider.set_label("Lacunarity: "+str((self.lac_slider.mark+10)/10))
             SETTINGS["lac"] = (self.lac_slider.mark+10)/10
 
-            self.x_offset_slider.set_label("X Offset: "+str(int(self.x_offset_slider.mark-300)))
-            SETTINGS["xof"] = self.x_offset_slider.mark - 300
-            self.y_offset_slider.set_label("Y Offset: "+str(int(self.y_offset_slider.mark-300)))
-            SETTINGS["yof"] = self.y_offset_slider.mark - 300
-
             pos = self.pos_entry.text.strip().split(',')
             for i, p in enumerate(pos):
                 if p != '':
@@ -616,7 +696,7 @@ def editor():
                     except:
                         pos[i] = 0
 
-            SETTINGS["pos"] = pos
+            SETTINGS["offset"] = pos
 
             pg.display.flip()
 
@@ -626,6 +706,12 @@ def editor():
                     if not in_settings.value:
                         quit_settings.value = 1
                         raise SystemExit
+                if event.type == pg.MOUSEWHEEL:
+                    if self.map_rect.collidepoint(pg.mouse.get_pos()):
+                        try:
+                            self.zoom_slider.set_mark(self.zoom_slider.mark-event.y)
+                        except:
+                            self.zoom_slider.set_mark(0)
 
     e = Editor()
     while True:
